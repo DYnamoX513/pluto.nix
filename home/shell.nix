cbb9edd760f596baa6b19ea66c1d82ef929dc40d
Yuxin Duan <yd2614@columbia.edu>
2024-12-18 17:44:50 +0800

    WIP: more comprehensive fish & zsh

--- home/shell.nix
+++ home/shell.nix
@@ -1,8 +1,139 @@
-{...}:
+{config, lib,pkgs,...}:
+let 
+    # miniconda initialize
+    condaDir = "${config.home.homeDirectory}/miniconda3";
+
+    condaFish = ''
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f ${condaDir}/bin/conda
+    eval ${condaDir}/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "${condaDir}/etc/fish/conf.d/conda.fish"
+        . "${condaDir}/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "${condaDir}/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+    '';
+
+    condaZsh = ''
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('${condaDir}/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "${condaDir}/etc/profile.d/conda.sh" ]; then
+        . "${condaDir}/etc/profile.d/conda.sh"
+    else
+        export PATH="${condaDir}/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+    '';
+
+    # cargo
+    cargo = shell:
+         if shell == "fish" then
+    "fish_add_path $HOME/.cargo/bin"
+        else
+            ''. "$HOME/.cargo/env"''
+        ;
+
+    # orbstack
+    orbstack = shell:
+        ''
+# Added by OrbStack: command-line tools and integration
+source ~/.orbstack/shell/init.${shell} 2>/dev/null || :
+    '';
+
+    # homebrew
+    homebrew = shell: ''eval "$(${config.homebrew.brewPrefix}/brew shellenv ${shell})"'';
+
+    # ~/Library/Application\ Support/JetBrains/Toolbox/scripts/... E.g., clion
+
+    # language - English
+    lang = shell: 
+    if shell == "fish" then
+        ''
+    set -gx LANG en_US.UTF-8
+    set -gx LC_CTYPE en_US.UTF-8
+    set -gx LC_ALL en_US.UTF-8
+    ''
+    else 
+        ''
+ export LANG="en_US.UTF-8"
+ export LC_CTYPE="en_US.UTF-8"
+ export LC_ALL="en_US.UTF-8"
+        '';
+
+    # wezterm completion
+    wezterm = shell: ''eval "$(wezterm shell-completion --shell ${shell})"'';
+
+        # brew + orbstack + cargo + language
+    commonLogin = shell :
+           [
+(orbstack shell)
+            (cargo shell)
+            (homebrew shell)
+            (lang shell)
+        ];
+    commonInteractive = shell:
+        [
+            (wezterm shell)
+        ]
+        ;
+
+    # fancy fish greeting
+in
 {
+
+    programs.zsh = {
+        enable = true;
+        # instead, use zsh-autocompletion https://github.com/marlonrichert/zsh-autocomplete
+        enableCompletion = false;
+        # zsh-autosuggestions
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+oh-my-zsh = {
+            enable = true;
+theme = "robbyrussell";
+            plugins = [
+"git"
+                # easily prefix your current or previous commands with sudo by pressing esc twice
+                "sudo"
+            ];
+        };
+        plugins=[
+  {
+    name = "zsh-autocomplete";
+    src = pkgs.fetchFromGitHub {
+      owner = "marlonrichert";
+      repo = "zsh-autocomplete";
+      rev = "24.09.04";
+      sha256 = "";
+    };
+  }
+
+];
+profileExtra = lib.strings.concatLines commonLogin "zsh";
+        initExtra = lib.strings.concatLines 
+            commonInteractive "zsh" ++ [
+            condaZsh
+        ];
+    };
 
     programs.fish = {
 enable = true;
+loginShellInit = lib.strings.concatLines commonLogin "fish";
+interactiveShellInit = 
+         lib.strings.concatLines 
+            commonInteractive "fish" ++ [
+                condaFish
+        ];
         functions = {
             # yazi change cwd
 yy = {
