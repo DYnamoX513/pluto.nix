771f4fded23e7036b1f396b7b1c87f63e268840e
Yuxin Duan <yd2614@columbia.edu>
2024-12-20 14:16:49 +0800

    Add helix

---
+++ home/helix.nix
@@ -0,0 +1,8 @@
+{config,...}:
+let
+  # the path to helix directory
+  configPath = ../share/helix;
+in {
+  xdg.configFile."helix".source = config.lib.file.mkOutOfStoreSymlink configPath;
+  programs.helix.enable = true;
+}
