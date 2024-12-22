459419710c88b747a7cf6ca4256c0a3c877cc037
Yuxin Duan <yd2614@columbia.edu>
2024-12-22 13:54:33 +0800

    WIP: fixing homebrew brew-only

--- hosts/Pluto-M3-Max/brew-only.nix
+++ hosts/Pluto-M3-Max/brew-only.nix
@@ -5,12 +5,20 @@ let
 brewNames = builtins.map (brew: brew.name) osConfig.homebrew.brews;
 caskNames = builtins.map (brew: brew.name) osConfig.homebrew.casks;
   homebrewPackages = brewNames ++ caskNames;
-
+isProgramInHomebrew= 
+    let
 # extra mappings brew -> HM
   hmToBrewPackages = {
-    "wezterm" = "wezterm";
     "wezterm@nightly" = "wezterm";
   };
+
+    # find all programs that are installed by homebrew but have different names in home-manager
+    extraPackages = builtins.filter (brew: builtins.elem brew homebrewPackages) (builtins.attrNames hmToBrewPackages);
+  
+  mergedPackages = homebrewPackages ++ (builtins.map (pkg: hmToBrewPackages.${pkg}) extraPackages);
+    in
+
+     prog: builtins.elem prog mergedPackages;
 
     mkGhostPackage = name: pkgs.emptyDirectory // {
           meta = pkgs.emptyDirectory.meta // {
@@ -18,33 +26,33 @@ caskNames = builtins.map (brew: brew.name) osConfig.homebrew.casks;
           };
         };
 
+
+ghostPackages = lib.mapAttrs 
+    (name: value: 
+      if  value ? package
+      then {
+        package = lib.mkOverride 999 (mkGhostPackage name);
+      }
+      else {})
+    (lib.filterAttrs
+     (
+    # some shell integrations require the fish package during the build
+                prog: _: prog != "fish" && isProgramInHomebrew prog
+            ) 
+      config.programs);
 in {
     # set each HM program's package to null
 
     ### This trick may not work for some packages...
     ### https://github.com/nix-community/home-manager/issues/4763 ###
 
-programs = lib.mapAttrs 
-    (name: value: 
-      if builtins.hasAttr "package" value
-      then {
-        package = lib.mkOverride 999 (mkGhostPackage name);
-      }
-      else {})
-    (lib.filterAttrs
-      (name: _:
-        builtins.any 
-          (pkg: pkg == name || 
-           (builtins.hasAttr name hmToBrewPackages && 
-            hmToBrewPackages.${name} == pkg)) 
-          homebrewPackages)
-      config.programs) // {
+programs = ghostPackages //   {
         # fzf needs a version to check shell integration
         # https://github.com/nix-community/home-manager/blob/1395379a7a36e40f2a76e7b9936cc52950baa1be/modules/programs/fzf.nix#L13
         # ```
         # hasShellIntegrationEmbedded = lib.versionAtLeast cfg.package.version "0.48.0";
         # ```
-            fzf.package = mkGhostPackage "fzf" // {version = "0.56.0";}; # homebrew's stable fzf
-        };
+            fzf.package = mkGhostPackage "fzf" // {version = "0.57.0";}; # homebrew's stable fzf (Dec 2024)
+        } ;
 
 }
