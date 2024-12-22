b511a1ca404da3b3739301c11637032692b320d1
Yuxin Duan <yd2614@columbia.edu>
2024-12-23 01:19:57 +0800

    MBP2018 specific brew casks

--- hosts/Pluto-i7-8750H/default.nix
+++ hosts/Pluto-i7-8750H/default.nix
@@ -13,6 +13,34 @@ let
   modules = {
     extra-modules = [
       # ./host-specifix.nix
+      (
+        {pkgs, ...}: {
+          homebrew = {
+            onActivation = {
+              # autoUpdate = true; # Fetch the newest stable branch of Homebrew's git repo
+              # upgrade = true; # Upgrade outdated casks, formulae, and App Store apps
+              # 'zap': uninstalls all formulae(and related files) not listed in the generated Brewfile
+              cleanup = "zap";
+            };
+
+            # `brew install --cask`
+            casks = [
+              # GPU-accelerated terminal emulator, written in Rust
+              "alacritty"
+              # Derived from IBM Plex Mono, with ligature
+              "font-lilex"
+              "font-lilex-nerd-font"
+              # Menu bar manager
+              "jordanbaird-ice"
+              "monitorcontrol"
+              # Eclipse Temurin 8
+              "temurin@8"
+              # Vivaldi browser
+              "vivaldi"
+            ];
+          };
+        }
+      )
     ];
     home-modules = [
       # ./home-specifix.nix
