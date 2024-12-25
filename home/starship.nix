96408af7d173d71c226131fb2cdf82ba33fed559
Yuxin Duan <yd2614@columbia.edu>
2024-12-25 16:15:27 +0800

    Update starship prompt

--- home/starship.nix
+++ home/starship.nix
@@ -1,11 +1,12 @@
-{...}: {
+{lib, ...}: {
   programs.starship = {
     enable = true;
 
     enableFishIntegration = true;
     enableZshIntegration = true;
+
     settings = {
-      format = "$all$directory$character";
+      format = "$directory$fill$all$line_break$shell$character";
       right_format = "$time";
       time = {
         disabled = false;
@@ -30,6 +31,13 @@
       shell = {
         disabled = false;
       };
+      conda = {
+        ignore_base = false;
+      };
     };
   };
+
+  home.activation.tipDisableCondaPS1 = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    echo "Starship does not suppress conda's own prompt modifier, you may want to run conda config --set changeps1 False"
+  '';
 }
--- hosts/Pluto-M3-Max/default.nix
+++ hosts/Pluto-M3-Max/default.nix
@@ -13,6 +13,38 @@ let
   modules = {
     extra-modules = [
       # ./host-specifix.nix
+      (
+        {pkgs, ...}: {
+          homebrew = {
+            onActivation = {
+              # autoUpdate = true; # Fetch the newest stable branch of Homebrew's git repo
+              # upgrade = true; # Upgrade outdated casks, formulae, and App Store apps
+              # 'zap': uninstalls all formulae(and related files) not listed in the generated Brewfile
+              cleanup = "zap";
+            };
+
+            taps = [
+              # "messense/macos-cross-toolchains"
+            ];
+
+            # `brew install`
+            brews = [
+              "go"
+              "pyenv" # python2
+              # "x86_64-unknown-linux-gnu" # cross compiler
+              "zellij" # terminal multiplexer
+            ];
+
+            # `brew install --cask`
+            casks = [
+              # Amazon OpenJDK 8 (arm64)
+              "corretto@8" # openjdk@8, temurin@8, etc. require x86_64 arch
+              # Eclipse Temurin
+              "temurin"
+            ];
+          };
+        }
+      )
     ];
     home-modules = [
       # ./home-specifix.nix
