905fe35f3b91dce756f79b75d8654f9b6041dd05
Yuxin Duan <yd2614@columbia.edu>
2025-01-10 21:48:23 +0800

    Change default editor to hx

--- modules/brew.nix
+++ modules/brew.nix
@@ -19,8 +19,8 @@
 
   # in global environment
   environment.variables = {
-    EDITOR = "nvim";
-    # EDITOR = "hx";
+    # EDITOR = "nvim";
+    EDITOR = "hx";
     LANG = "en_US.UTF-8";
     LC_CTYPE = "en_US.UTF-8";
     LC_ALL = "en_US.UTF-8";
